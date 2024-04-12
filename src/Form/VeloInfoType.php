f08eb3f6f8c97f3704c3344295964ba3609ef629
Adam Maudoux <adammaudoux@gmail.com>
2024-04-12 18:38:28 +0200

    feat : ajout du form VeloInfo

---
+++ src/Form/VeloInfoType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Velo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VeloInfoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('numero_de_serie')
+            ->add('marque')
+            ->add('ref_recyclerie')
+            ->add('couleur')
+            ->add('poids')
+            ->add('taille_roues')
+            ->add('taille_cadre')
+            ->add('etat')
+            ->add('url_photo')
+            ->add('date_de_réception', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('date_de_vente', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('type')
+            ->add('année')
+            ->add('emplacement')
+            ->add('commentaire')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Velo::class,
+        ]);
+    }
+}
