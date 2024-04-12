8878ee576b3e16e9ff57d493e4969e31ef330981
Adam Maudoux <adammaudoux@gmail.com>
2024-04-12 20:16:34 +0200

    feat le formulaire basique poste en bdd mais n'a pas de mise en forme

--- src/Controller/VeloController.php
+++ src/Controller/VeloController.php
@@ -10,18 +10,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class VeloController extends AbstractController
 {
     #[Route('/velo/new', name: 'velo_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $velo = new Velo();
         $form = $this->createForm(VeloInfoType::class, $velo);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($velo);
             $entityManager->flush();
 
