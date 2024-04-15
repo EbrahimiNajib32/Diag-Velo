3c4c2e3c0769ec39b8f7b4a085bd6d19d2267e83
Liz <manresa.liz@gmail.com>
2024-04-15 15:47:34 +0200

    fix: Fixed entity, created form and controller

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $role = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $informations_de_contact = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getRole(): ?int
+    {
+        return $this->role;
+    }
+
+    public function setRole(int $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getInformationsDeContact(): ?string
+    {
+        return $this->informations_de_contact;
+    }
+
+    public function setInformationsDeContact(?string $informations_de_contact): static
+    {
+        $this->informations_de_contact = $informations_de_contact;
+
+        return $this;
+    }
+}
