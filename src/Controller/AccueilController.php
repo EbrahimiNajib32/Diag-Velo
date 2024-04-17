6d04c9bfc25f2f32d1619cd7d3d46a45624abe0c
girard.mehdi <mehdi_girard@orange.fr>
2024-04-17 15:28:21 +0200

    feat ajout bouton rechercher par ref recyclerie dans la page accueil plus methode dans le controlleur pour rechercher la ref recyclerie plus front pour afficher le bouton

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,18 +1,46 @@
 <?php
-
+// Importer les classes nécessaires
 namespace App\Controller;
 
+use App\Entity\Velo;
+use App\Form\SearchVeloType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Créer le formulaire de recherche de vélo
+        $searchForm = $this->createForm(SearchVeloType::class);
+        $searchForm->handleRequest($request);
+
+        // Vérifier si le formulaire est soumis et valide
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            // Récupérer la référence de recyclérie saisie par l'utilisateur
+            $refRecyclerie = $searchForm->getData()['ref_recyclerie_search'];
+
+            // Rechercher le vélo correspondant dans la base de données
+            $velo = $this->getDoctrine()->getRepository(Velo::class)->findOneBy(['refRecyclerie' => $refRecyclerie]);
+
+            // Si un vélo correspondant est trouvé, passer ses informations au template
+            if ($velo) {
+                return $this->render('accueil/velo_details.html.twig', [
+                    'velo' => $velo,
+                ]);
+            } else {
+                // Gérer le cas où aucun vélo n'est trouvé avec la référence de recyclérie saisie
+                // Par exemple, afficher un message d'erreur ou rediriger vers une autre page
+            }
+        }
+
+        // Si le formulaire n'a pas été soumis ou n'est pas valide, afficher la page d'accueil avec le formulaire
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'searchForm' => $searchForm->createView(),
         ]);
     }
 }
--- src/Controller/VeloController.php
+++ src/Controller/VeloController.php
@@ -1,13 +1,10 @@
 <?php
-
-// src/Controller/VeloController.php
 // src/Controller/VeloController.php
 
 namespace App\Controller;
 
 use App\Entity\Velo;
 use App\Form\VeloInfoType;
-use App\Form\SearchVeloType; // Importez le formulaire de recherche
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,15 +28,8 @@ class VeloController extends AbstractController
             return $this->redirectToRoute('velo_success');
         }
 
-        // Créer le formulaire de recherche
-        $searchForm = $this->createForm(SearchVeloType::class);
-        $searchForm->handleRequest($request);
-
-        // Ajouter ici la logique pour la recherche si nécessaire
-
         return $this->render('velo/new.html.twig', [
             'form' => $form->createView(),
-            'searchForm' => $searchForm->createView(), // Passer le formulaire de recherche au template Twig
         ]);
     }
 }
