ec80aeb08f31c4639932325e81e7a9780a333c3c
moukhliss.adam <adam.moukhliss78@gmail.com>
2024-04-17 18:38:45 +0200

    WIP - Création entité table relationnelle

---
+++ migrations/Version20240417144607.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240417144607 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE diagnostic_element (id INT AUTO_INCREMENT NOT NULL, id_diagnostic INT NOT NULL, id_element INT NOT NULL, commentaire VARCHAR(255) DEFAULT NULL, id_etat INT NOT NULL, INDEX IDX_BCE100665FF6085E (id_diagnostic), INDEX IDX_BCE100669FDDF749 (id_element), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE diagnostic_element ADD CONSTRAINT FK_BCE100665FF6085E FOREIGN KEY (id_diagnostic) REFERENCES diagnostic (id)');
+        $this->addSql('ALTER TABLE diagnostic_element ADD CONSTRAINT FK_BCE100669FDDF749 FOREIGN KEY (id_element) REFERENCES element_control (id)');
+        $this->addSql('ALTER TABLE velo ADD proprietaire_id INT NOT NULL');
+        $this->addSql('ALTER TABLE velo ADD CONSTRAINT FK_354971F576C50E4A FOREIGN KEY (proprietaire_id) REFERENCES proprietaire (id)');
+        $this->addSql('CREATE INDEX IDX_354971F576C50E4A ON velo (proprietaire_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE diagnostic_element DROP FOREIGN KEY FK_BCE100665FF6085E');
+        $this->addSql('ALTER TABLE diagnostic_element DROP FOREIGN KEY FK_BCE100669FDDF749');
+        $this->addSql('DROP TABLE diagnostic_element');
+        $this->addSql('ALTER TABLE velo DROP FOREIGN KEY FK_354971F576C50E4A');
+        $this->addSql('DROP INDEX IDX_354971F576C50E4A ON velo');
+        $this->addSql('ALTER TABLE velo DROP proprietaire_id');
+    }
+}
--- migrations/Version20240417144712.php
+++ migrations/Version20240417144712.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240417121326 extends AbstractMigration
+final class Version20240417144712 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,9 @@ final class Version20240417121326 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE proprietaire (id INT AUTO_INCREMENT NOT NULL, nom_proprio VARCHAR(255) NOT NULL, telephone INT NOT NULL, email VARCHAR(500) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE element_control ADD id INT AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)');
+        $this->addSql('CREATE TABLE diagnostic_element (id INT AUTO_INCREMENT NOT NULL, id_diagnostic INT NOT NULL, id_element INT NOT NULL, commentaire VARCHAR(255) DEFAULT NULL, id_etat INT NOT NULL, INDEX IDX_BCE100665FF6085E (id_diagnostic), INDEX IDX_BCE100669FDDF749 (id_element), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE diagnostic_element ADD CONSTRAINT FK_BCE100665FF6085E FOREIGN KEY (id_diagnostic) REFERENCES diagnostic (id)');
+        $this->addSql('ALTER TABLE diagnostic_element ADD CONSTRAINT FK_BCE100669FDDF749 FOREIGN KEY (id_element) REFERENCES element_control (id)');
         $this->addSql('ALTER TABLE velo ADD proprietaire_id INT NOT NULL');
         $this->addSql('ALTER TABLE velo ADD CONSTRAINT FK_354971F576C50E4A FOREIGN KEY (proprietaire_id) REFERENCES proprietaire (id)');
         $this->addSql('CREATE INDEX IDX_354971F576C50E4A ON velo (proprietaire_id)');
@@ -30,12 +31,10 @@ final class Version20240417121326 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE diagnostic_element DROP FOREIGN KEY FK_BCE100665FF6085E');
+        $this->addSql('ALTER TABLE diagnostic_element DROP FOREIGN KEY FK_BCE100669FDDF749');
+        $this->addSql('DROP TABLE diagnostic_element');
         $this->addSql('ALTER TABLE velo DROP FOREIGN KEY FK_354971F576C50E4A');
-        $this->addSql('DROP TABLE proprietaire');
-        $this->addSql('ALTER TABLE element_control MODIFY id INT NOT NULL');
-        $this->addSql('DROP INDEX `PRIMARY` ON element_control');
-        $this->addSql('ALTER TABLE element_control DROP id');
-        $this->addSql('ALTER TABLE element_control ADD PRIMARY KEY (id_element)');
         $this->addSql('DROP INDEX IDX_354971F576C50E4A ON velo');
         $this->addSql('ALTER TABLE velo DROP proprietaire_id');
     }
