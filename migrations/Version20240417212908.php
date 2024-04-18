bfffafed89b143242fa8f713fe63adb84a84028c
Adam Maudoux <adammaudoux@gmail.com>
2024-04-18 09:31:20 +0200

    chore : correction de liaison bdd

--- migrations/Version20240417212908.php
+++ migrations/Version20240417212908.php
@@ -21,7 +21,7 @@ final class Version20240417212908 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE etat_control (id INT AUTO_INCREMENT NOT NULL, nom_etat VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE etat_control ADD CONSTRAINT FK_FA7C8889BE696DF2 FOREIGN KEY (id) REFERENCES diagnostic (id)');
+        $this->addSql('ALTER TABLE etat_control ADD CONSTRAINT FK_FA7C8889BE696DF2 FOREIGN KEY (id) REFERENCES diagnostic_element (id)');
     }
 
     public function down(Schema $schema): void
