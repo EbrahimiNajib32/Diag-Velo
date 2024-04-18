8ff9aaf6f2e0c70a9747e8ee19f6ee959d1b9cf8
girard.mehdi <mehdi_girard@orange.fr>
2024-04-18 20:03:53 +0200

    feat ajout de nouvelle table proprietaire plus champ proprietaire dans velo correction le liaison avec bdd dans controlleur wip

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,9 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface; // Gardez cette ligne ici, une seule fois
 
 class AccueilController extends AbstractController
 {
+    // Route pour la page d'accueil
     #[Route('/', name: 'app_accueil')]
     public function index(Request $request): Response
     {
@@ -20,21 +22,16 @@ class AccueilController extends AbstractController
 
         // Vérifier si le formulaire est soumis et valide
         if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-            // Récupérer la référence de recyclérie saisie par l'utilisateur
             $refRecyclerie = $searchForm->getData()['ref_recyclerie_search'];
 
-            // Rechercher le vélo correspondant dans la base de données
             $entityManager = $this->getDoctrine()->getManager();
             $velo = $entityManager->getRepository(Velo::class)->findOneBy(['ref_recyclerie' => $refRecyclerie]);
 
-            // Si un vélo correspondant est trouvé, passer ses informations au template
             if ($velo) {
                 return $this->render('accueil/velo_details.html.twig', [
                     'velo' => $velo,
                 ]);
             } else {
-                // Gérer le cas où aucun vélo n'est trouvé avec la référence de recyclérie spécifiée
-                // Par exemple, afficher un message d'erreur ou rediriger vers une autre page
                 return $this->render('accueil/index.html.twig', [
                     'controller_name' => 'AccueilController',
                     'searchForm' => $searchForm->createView(),
@@ -43,30 +40,23 @@ class AccueilController extends AbstractController
             }
         }
 
-        // Si le formulaire n'a pas été soumis ou n'est pas valide, afficher la page d'accueil avec le formulaire
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             'searchForm' => $searchForm->createView(),
         ]);
     }
 
+    // Route pour les détails du vélo
     #[Route('/velo/details/{ref_recyclerie?}', name: 'velo_details')]
-    public function veloDetails($ref_Recyclerie = null): Response
+    public function veloDetails(EntityManagerInterface $entityManager, $ref_recyclerie = null): Response
     {
-        // Obtenez le gestionnaire d'entités de Doctrine en utilisant $this->getDoctrine()
-        $entityManager = $this->getDoctrine()->getManager();
+        $velo = $entityManager->getRepository(Velo::class)->findOneBy(['ref_recyclerie' => $ref_recyclerie]);
 
-        // Recherchez le vélo correspondant dans la base de données en fonction de la référence de recyclérie
-        $velo = $entityManager->getRepository(Velo::class)->findOneBy(['ref_recyclerie' => $refRecyclerie]);
-
-        // Si un vélo correspondant est trouvé, passez ses informations au template
         if ($velo) {
             return $this->render('accueil/velo_details.html.twig', [
                 'velo' => $velo,
             ]);
         } else {
-            // Gérer le cas où aucun vélo n'est trouvé avec la référence de recyclérie spécifiée
-            // Par exemple, afficher un message d'erreur ou rediriger vers une autre page
             return $this->render('accueil/velo_details.html.twig', [
                 'error_message' => 'Aucun vélo trouvé avec la référence de recyclérie spécifiée.',
             ]);
