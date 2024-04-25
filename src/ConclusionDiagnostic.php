bd0b1ba3ac68a1427ff84c7ba6c05222cb18ec69
EbrahimiNajib32 <157003674+EbrahimiNajib32@users.noreply.github.com>
2024-04-25 15:03:54 +0200

    VELO-130-feat-Added-new-class-enumeration-in-project

--- src/ConclusionDiagnostic.php
+++ src/ConclusionDiagnostic.php
@@ -1,1 +1,12 @@
 <?php
+
+// src/Enum/ConclusionDiagnostic.php
+
+namespace App\Enum;
+
+class ConclusionDiagnostic
+{
+    const RAS = 'R.A.S';
+    const A_REPARER = 'à réparer';
+    const POUR_PIECES = 'pour pièces';
+}
