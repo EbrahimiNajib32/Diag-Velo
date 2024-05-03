d0ecd2c3f3bbadb41bfc5ca7485c9fbccc99e1bc
girard.mehdi <mehdi_girard@orange.fr>
2024-05-03 16:02:42 +0200

    feat : Ajout de la méthode POST pour l'envoi de nouveaux elements (controleur), ajout du formulaire et du fichier Twig pour la vue

---
+++ src/Form/ElementControlCreatType.php
@@ -0,0 +1,32 @@
+<?php
+
+// src/Form/ElementControlCreateType.php
+namespace App\Form;
+
+use App\Entity\ElementControl;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ElementControlCreateType extends AbstractType
+
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('element', TextType::class, [
+                'label' => 'Élément',
+                'attr' => ['placeholder' => 'Entrez le nom de l\'élément']
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Créer Élément']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ElementControl::class,
+        ]);
+    }
+}
