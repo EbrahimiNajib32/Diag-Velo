530e352b2fd21f79986040dd8ec8ef3c7acf75f4
Liz <manresa.liz@gmail.com>
2024-05-16 18:17:27 +0200

    feat: display type of diagnostic's list

--- src/Controller/TypeDiagnosticController.php
+++ src/Controller/TypeDiagnosticController.php
@@ -12,14 +12,24 @@ use Doctrine\ORM\EntityManagerInterface;
 class TypeDiagnosticController extends AbstractController
 {
     #[Route('/type/diagnostic', name: 'app_type_diagnostic')]
-public function index(EntityManagerInterface $entityManager): Response
-// Récupérer uniquement les types de diagnostic actifs
-{
-    $typesDiagnostic = $entityManager->getRepository(DiagnosticType::class)->findBy(['actif' => true]);
+    public function index(EntityManagerInterface $entityManager): Response
+    // Récupérer uniquement les types de diagnostic actifs
+    {
+        $typesDiagnostic = $entityManager->getRepository(DiagnosticType::class)->findBy(['actif' => true]);
 
 
-    return $this->render('diagnostic/choixTypeDiagnostic.html.twig', [
-        'typesDiagnostic' => $typesDiagnostic,
-    ]);
-}
+        return $this->render('diagnostic/choixTypeDiagnostic.html.twig', [
+            'typesDiagnostic' => $typesDiagnostic,
+        ]);
+    }
+
+    #[Route('/dashboard/typediagnostic', name: 'app_type_diagnostic_liste')]
+    public function listeComplete(EntityManagerInterface $entityManager): Response
+    {
+        $typesDiagnostic = $entityManager->getRepository(DiagnosticType::class)->findAll();
+
+        return $this->render('type_diagnostic/liste.html.twig', [
+            'typesDiagnostic' => $typesDiagnostic,
+        ]);
+    }
 }
