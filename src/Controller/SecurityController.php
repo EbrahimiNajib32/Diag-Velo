2aafb3bc084ddba6c03d6775f456e8a965139a83
EbrahimiNajib32 <157003674+EbrahimiNajib32@users.noreply.github.com>
2024-05-22 11:33:18 +0200

    VELO-172-Modifications-Page-Security-and-SecurityController-Update.PHP8-idea.xml

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,9 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        public function accessDenied(): Response
+    {
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
