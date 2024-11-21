241486f27261aa6305e8d14f34a6fa7d9bd73660
julien-muller <mullerjulien995@gmail.com>
2024-11-21 10:45:16 +0100

    fix: Supression d'une ligne en trop dans le fichier typediagnosticcontroller du a un conflit git fix du bug

--- src/Controller/TypeDiagnosticController.php
+++ src/Controller/TypeDiagnosticController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
