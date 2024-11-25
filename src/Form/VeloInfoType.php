6ed85488e11acf2d2fdd67b966e2f525356d22e2
jean-luc Vassal <jean-luc.vassal@uha.fr>
2024-11-25 10:42:47 +0100

    feat affichage page détails vélos

--- src/Form/VeloInfoType.php
+++ src/Form/VeloInfoType.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Form;
-use App\Entity\Proprietaire; // Make sure this is correct
+
+use App\Entity\Proprietaire;
 use App\Entity\Velo;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -21,9 +21,7 @@ use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\File;
 
 class VeloInfoType extends AbstractType
 {
@@ -36,31 +34,22 @@ class VeloInfoType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-
         $builder
-            ->add('couleur', HiddenType::class, [
-                'mapped' => false,
-            ])
-            ->add('ref_recyclerie', TextType::class , [
+            ->add('ref_recyclerie', TextType::class, [
                 'required' => false,
                 'label' => 'Référence Recyclerie'
             ])
-
-           ->add('bicycode', TextType::class, [
-               'required' => false,
+            ->add('bicycode', TextType::class, [
+                'required' => false,
                 'label' => 'Bicycode'
-           ])
-
+            ])
             ->add('marque', TextType::class, [
                 'label' => 'Marque'
             ])
-
-
-            ->add('numero_de_serie', TextType::class , [
+            ->add('numero_de_serie', TextType::class, [
                 'required' => false,
                 'label' => 'Numéro de série'
             ])
-
             ->add('etat', ChoiceType::class, [
                 'required' => true,
                 'choices' => [
@@ -69,20 +58,17 @@ class VeloInfoType extends AbstractType
                     'moyen' => 'moyen',
                     'mauvais' => 'mauvais',
                     'très mauvais' => 'très mauvais',
-
                 ],
                 'label' => 'Etat'
             ])
-
-            ->add('model', TextType::class, [  // Ajout du champ modèle
+            ->add('model', TextType::class, [
                 'required' => false,
                 'label' => 'Modèle'
             ])
-
-            ->add('poids' , TextType::class , [
-                        'required' => false,
-                        'label' => 'Poids'
-                        ])
+            ->add('poids', TextType::class, [
+                'required' => false,
+                'label' => 'Poids'
+            ])
             ->add('taille_roues', ChoiceType::class, [
                 'choices' => [
                     '12 pouces (203 mm)' => '12',
@@ -102,7 +88,7 @@ class VeloInfoType extends AbstractType
                     '32 pouces (ISO 686)' => '32',
                     '36 pouces (914 mm)' => '36'
                 ],
-                'label' => 'Taille des roues',
+                'label' => 'Taille des roues'
             ])
             ->add('taille_cadre', IntegerType::class, [
                 'required' => false,
@@ -112,15 +98,12 @@ class VeloInfoType extends AbstractType
                 'label' => 'Photo',
                 'mapped' => false,
                 'required' => false,
-                'attr' => ['id' => 'url_photo'],  // Ensure this attribute is set
+                'attr' => ['id' => 'url_photo'],
             ])
-
-
             ->add('origine', TextType::class, [
-                  'required' => false,
-                   'label' => 'Origine du vélo'
-             ])
-
+                'required' => false,
+                'label' => 'Origine du vélo'
+            ])
             ->add('type', ChoiceType::class, [
                 'choices' => [
                     'BMX' => 'BMX',
@@ -144,24 +127,23 @@ class VeloInfoType extends AbstractType
                     'Trotinette' => 'Trotinette',
                     'Trotinette électrique' => 'Trotinette électrique',
                     'Autres' => 'Autres',
-
                 ],
-                'label' => 'Type de vélo',
+                'label' => 'Type de vélo'
             ])
-            ->add('public', ChoiceType::class , [
-                    'choices' => [
-                        'Homme' => 'Homme',
-                        'Femme' => 'Femme',
-                        'Unisex' => 'Unisex',
-                        'Enfant' => 'Enfant',
-                    ],
-                    'label' => 'Public',]
-            )
-            ->add('commentaire', TextType::class , [
+            ->add('public', ChoiceType::class, [
+                'choices' => [
+                    'Homme' => 'Homme',
+                    'Femme' => 'Femme',
+                    'Unisex' => 'Unisex',
+                    'Enfant' => 'Enfant',
+                ],
+                'label' => 'Public'
+            ])
+            ->add('commentaire', TextType::class, [
                 'required' => false,
                 'label' => 'Commentaire'
             ])
-            ->add('emplacement', TextType::class , [
+            ->add('emplacement', TextType::class, [
                 'required' => false,
                 'label' => 'Emplacement'
             ])
@@ -174,10 +156,9 @@ class VeloInfoType extends AbstractType
                 'mapped' => false,
                 'label' => 'Choisir ou Ajouter Proprietaire',
                 'choice_attr' => function($choice, $key, $value) {
-                    return ['class' => 'choice-option']; // Add a class to each option
+                    return ['class' => 'choice-option'];
                 }
             ])
-
             ->add('autocomplete_proprietaire', TextType::class, [
                 'required' => false,
                 'mapped' => false,
@@ -191,7 +172,7 @@ class VeloInfoType extends AbstractType
                 'choice_label' => 'nomProprio',
                 'required' => false,
                 'placeholder' => 'Sélectionner un propriétaire',
-                'attr' => ['style' => 'display:none;'], // Hide this field, as it's set through autocomplete
+                'attr' => ['style' => 'display:none;'],
             ])
             ->add('nom_proprio', TextType::class, [
                 'mapped' => false,
@@ -202,13 +183,9 @@ class VeloInfoType extends AbstractType
                 'mapped' => false,
                 'required' => false,
                 'attr' => ['placeholder' => 'Email ..', 'style' => 'display: none;'],
-                'constraints' => [
-                    new Email(),
-                ],
-
+                'constraints' => [new Email()],
             ])
-
-            ->add('telephone', TelType::class, [ //Type spéciale pour téléphone ouvre un pavé numérique a la place d'un clavier complet
+            ->add('telephone', TelType::class, [
                 'mapped' => false,
                 'required' => false,
                 'attr' => [
@@ -217,7 +194,6 @@ class VeloInfoType extends AbstractType
                     'maxlength' => 15,
                 ],
             ])
-
             ->add('statut', ChoiceType::class, [
                 'choices' => [
                     'Particulier' => 'Particulier',
@@ -225,122 +201,32 @@ class VeloInfoType extends AbstractType
                 ],
                 'mapped' => false,
                 'label' => 'Statut',
-                'required' => true            
+                'required' => true
             ]);
 
-        $builder->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) {
-                $form = $event->getForm();
-                $velo = $form->getData();
-                $proprietaire = $velo->getProprietaire();
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+            $form = $event->getForm();
+            $velo = $form->getData();
+            $proprietaire = $velo->getProprietaire();
 
-                if (!$proprietaire) {
+            if (!$proprietaire) {
+                $nomProprio = $form->get('nom_proprio')->getData();
+                $email = $form->get('email')->getData();
+                $telephone = $form->get('telephone')->getData();
+                $statut = $form->get('statut')->getData();
 
-                    $nomProprio = $form->get('nom_proprio')->getData();
-                    $email = $form->get('email')->getData();
-                    $telephone = $form->get('telephone')->getData();
-                    $statut = $form->get('statut')->getData();
-
-                    if (!empty($nomProprio) || !empty($email) || !empty($telephone) || !empty($statut)) {
-                        $newProprietaire = new Proprietaire();
-                        $newProprietaire->setNomProprio($nomProprio);
-                        $newProprietaire->setEmail($email);
-                        $newProprietaire->setTelephone($telephone);
-                        $newProprietaire->setStatut($statut);
-
-                        $this->entityManager->persist($newProprietaire);
-                        $this->entityManager->flush();
-
-
-                        $velo->setProprietaire($newProprietaire);
-                    }
-                } else {
-
-                    if (!empty($proprietaire->getNomProprio()) || !empty($proprietaire->getEmail()) || !empty($proprietaire->getTelephone()) || !empty($proprietaire->getStatut())) {
-
-                        $proprietaire->setNomProprio($proprietaire->getNomProprio());
-                        $proprietaire->setEmail($proprietaire->getEmail());
-                        $proprietaire->setTelephone($proprietaire->getTelephone());
-                        $proprietaire->setStatut($proprietaire->getStatut());
-
-                        $this->entityManager->flush();
-                    }
+                if (!empty($nomProprio) || !empty($email) || !empty($telephone) || !empty($statut)) {
+                    $newProprietaire = new Proprietaire();
+                    $newProprietaire->setNomProprio($nomProprio);
+                    $newProprietaire->setEmail($email);
+                    $newProprietaire->setTelephone($telephone);
+                    $newProprietaire->setStatut($statut);
+                    $this->entityManager->persist($newProprietaire);
+                    $this->entityManager->flush();
+                    $velo->setProprietaire($newProprietaire);
                 }
             }
-        );
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-            $velo = $event->getData();
-            $form = $event->getForm();
-            if ($velo && $velo->getCouleur()) {
-                $colors = explode('&', $velo->getCouleur());
-                $form->get('couleur')->setData(join(',', $colors));
-            }
         });
-
-        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
-            $form = $event->getForm();
-            $data = $form->get('couleur')->getData();
-            if ($data) {
-                $colors = implode('&', $data);
-                $event->getData()->setCouleur($colors);
-            }
-        });
-        $this->addColorCheckboxes($builder);
-    }
-
-
-    private function getBrandChoices()
-    {
-        $brands = [
-            'Autres', 'Basso', 'BH', 'BMC', 'Boardman Bikes', 'Breezer', 'Brooklyn Bicycle Co.',
-            'Canyon', 'Cinelli', 'Cleary Bikes', 'Colnago', 'Commençal', 'Co-op Cycles (REI)', 'Dahon', 'De Rosa',
-            'Diamondback', 'Dynacraft', 'Eddy Merckx', 'Electra', 'Evil Bike Co.', 'Fairdale', 'Felt', 'Focus',
-            'Fuji', 'Ghost', 'GT', 'Holdsworth', 'Huffy', 'Ibis', 'Jamis', 'Kona', 'Lapierre', 'LeMond', 'Linus',
-            'Look', 'Marin', 'Masi', 'Mercier', 'Mikado', 'Mongoose', 'Mondraker', 'Norco', 'Orange Bikes', 'Orient Bikes',
-            'Orbea', 'Pashley Cycles', 'Peugeot', 'Pinarello', 'Pivot', 'Planet X', 'Polygon', 'Public Bikes', 'Quintana Roo',
-            'Rabeneick', 'Raleigh', 'Ridley', 'Ritte', 'Rocky Mountain', 'Salsa', 'Saracen', 'Schwinn', 'Sekine', 'Serotta',
-            'Simcoe', 'Sole Bicycles', 'Sommer', 'Soma', 'Spot Brand', 'Staiger', 'State Bicycle Co.', 'Storck', 'Strider',
-            'Stromer', 'Surly', 'Tern', 'Throne Cycles', 'Tokyobike', 'Torpado', 'Transition', 'Union', 'Van Nicholas',
-            'VanMoof', 'Veloretti', 'Vitus', 'Wilier Triestina', 'Woom', 'Yeti Cycles', 'Zinn'
-        ];
-        asort($brands);
-        return array_combine($brands, $brands);
-    }
-
-
-
-    private function addColorCheckboxes(FormBuilderInterface $builder)
-    {
-        $builder->add('couleur', ChoiceType::class, [
-            'choices' => [
-                'Blanc' => 'white',
-                'Beige' => 'beige',
-                'Saumon' => 'darksalmon',
-                'Rose' => 'pink',
-                'Jaune' => 'yellow',
-                'Or' => 'gold',
-                'Orange' => 'orange',
-                'Rouge' => 'red',
-                'Kaki' => 'khaki',
-                'Vert fluo' => 'lime',
-                'Vert' => 'green',
-                'Olive' => 'olive',
-                'Bleu océan' => 'aqua',
-                'Bleu' => 'blue',
-                'Bleu foncé' => 'navy',
-                'Violet' => 'violet',
-                'Brun' => 'brown',
-                'Gris' => 'grey',
-                'Noir' => 'black',
-                'Argent' => 'silver'
-            ],
-            'expanded' => true,
-            'multiple' => true,
-            'mapped' => false,
-            'required' => false,
-            'label' => 'Couleurs (multiples)'
-        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
